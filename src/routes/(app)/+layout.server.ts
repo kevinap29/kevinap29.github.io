48937df037433270da83417f176fe197adc62274
kevinap29 <kevin.agustiansyah@grcboard.com>
2024-06-10 11:33:45 +0700

    revert use of api

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,9 +1,8 @@
+import { listTheme } from '$lib/data/theme'
 import type { LayoutServerLoad } from './$types';
-import type { HttpResult, SkeletonTheme } from '$lib/@types';
 
 export const load = (async ({ cookies, fetch, url }) => {
     let appTheme: string = '';
-    let listTheme: SkeletonTheme[] = [];
 
     try {
         appTheme = cookies.get('APP_THEME') || 'wintry'
@@ -11,19 +10,6 @@ export const load = (async ({ cookies, fetch, url }) => {
         const error = e as Error;
 
         appTheme = ''
-    }
-
-    try {
-        const response = await fetch(`${url.origin}/api/public/theme`, {
-            method: 'GET'
-        })
-        const data = await response.json() as HttpResult<SkeletonTheme[]>;
-        
-        if (data.status != 200) throw Error();
-
-        listTheme = data.value;
-    } catch (e: unknown) {
-        listTheme = [];
     }
 
     return {
