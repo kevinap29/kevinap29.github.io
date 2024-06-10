896463604cb5e95fc562830c61ab7bf76d86c69f
kevinap29 <kevin.agustiansyah@grcboard.com>
2024-06-10 11:50:09 +0700

    remove page error

--- src/routes/(error)/error/+page.svelte
+++ src/routes/(error)/error/+page.svelte
@@ -3,17 +3,15 @@
     import { goto } from '$app/navigation'
     import ArrowLeftOutline from 'flowbite-svelte-icons/ArrowLeftOutline.svelte'
 
-    $: queryStatus = $page.url.searchParams.get('status')
-    $: queryMessage = $page.url.searchParams.get('message')
-    
-    let goBackMessage = "<-- Go Back";
+    // $: queryStatus = $page.url.searchParams.get('status')
+    // $: queryMessage = $page.url.searchParams.get('message')
 </script>
 
 <div class="flex justify-center items-center h-full mx-auto">
     <div class="card size-2/4 flex justify-center items-center mx-auto">
         <section class="p-4 text-center space-y-8">
-            <p class="h1 font-bold">{queryStatus}</p>
-            <p class="h3">{queryMessage}</p>
+            <!-- <p class="h1 font-bold">{queryStatus}</p>
+            <p class="h3">{queryMessage}</p> -->
             <button class="btn variant-glass-tertiary space-x-3" on:click={async () => await goto($page.url.origin)}>
                 <ArrowLeftOutline /> 
                 Go Back
