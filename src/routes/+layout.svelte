275b35a212bad19e60176bc077e334d294720e02
kevinap29 <kevinagustiansyah298@gmail.com>
2025-07-12 12:20:40 +0700

    feat: add sidebar component with various subcomponents and styles
    - Introduced a new sidebar component structure with multiple subcomponents including Content, Footer, Group, Header, Input, Menu, and more.
    - Implemented sidebar functionality with context management for open/close states.
    - Added responsive design features for mobile and desktop views.
    - Integrated tooltip functionality for sidebar items.
    - Created skeleton loading states for sidebar items.
    - Enhanced accessibility features with appropriate ARIA attributes.
    - Established a new hook for mobile detection.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
 	import '../app.css';
 
+	import { ModeWatcher, toggleMode  } from "mode-watcher";
+
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import AppSidebar from "$lib/components/app-sidebar.svelte";
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<ModeWatcher />
+
+<Sidebar.Provider >
+	<AppSidebar />
+	<main>
+		<Sidebar.Trigger />
+		{@render children?.()}
+	</main>
+</Sidebar.Provider>
